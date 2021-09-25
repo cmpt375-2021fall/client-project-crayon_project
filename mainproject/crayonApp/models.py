547e3fe33f637e39a489cd35ada3a8582626ad7e
MoonTea0416 <winter0416@hotmail.com>
2021-09-24 23:47:01 -0600

    add 4 tables on modules

--- mainproject/crayonApp/models.py
+++ mainproject/crayonApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+
+class QuizType(models.Model):
+      name =  models.CharField(max_length=50)
+class Quiz(models.Model):
+    choice_text = models.CharField(max_length=200)
+    type =  models.ForeignKey(QuizType, on_delete=models.CASCADE)
+
+class Response(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+ 
