1645d340b87fc8c6db0b47133c94d41e0c5565a2
MoonTea0416 <winter0416@hotmail.com>
2021-09-28 12:33:22 -0600

    add super user

--- mainproject/crayonApp/models.py
+++ mainproject/crayonApp/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
+    username = models.CharField(max_length=100, null=False)
+    password = models.CharField(max_length=50, null=False)
     email = models.EmailField()
 
 class QuizType(models.Model):
-      name =  models.CharField(max_length=50)
+    name =  models.CharField(max_length=50, null=False)
+
 class Quiz(models.Model):
     choice_text = models.CharField(max_length=200)
     type =  models.ForeignKey(QuizType, on_delete=models.CASCADE)
 
 class Response(models.Model):
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200, default = "Perfect")
     votes = models.IntegerField(default=0)
 
  
--- mainproject/mainproject/settings.py
+++ mainproject/mainproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crayonApp.apps.CrayonappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
