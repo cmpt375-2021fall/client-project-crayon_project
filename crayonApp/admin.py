7af8eade4f8a7df3616d8ae24618a1442d4b7961
wyntermindnich <wkm0123@westminstercollege.edu>
2021-10-07 12:26:25 -0600

    HEROKU upload

--- crayonApp/admin.py
+++ crayonApp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import QuizType, Quiz
+from .models import QuizType, Quiz, QuizSubtype
 
 admin.site.register(QuizType)
 admin.site.register(Quiz)
-
+admin.site.register(QuizSubtype)
