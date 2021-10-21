2f3673b23ec81070403a4e6432cd46676afa6005
wyntermindnich <wkm0123@westminstercollege.edu>
2021-10-21 13:09:47 -0600

    fixing my branch

--- crayonApp/admin.py
+++ crayonApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import QuizType, Quiz, QuizSubtype
+from .models import QuizType, Quiz, QuizSubtype, User
 
 admin.site.register(QuizType)
 admin.site.register(Quiz)
 admin.site.register(QuizSubtype)
+admin.site.register(User)
