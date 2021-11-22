8b51686c8998ed5911f1f8602f8e3524aad98649
MoonTea0416 <winter0416@hotmail.com>
2021-11-22 10:12:34 -0700

    change configure to handle deploy issue

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "crayonApp/static"), ]
 
 # specify media root for user uploaded files,
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
