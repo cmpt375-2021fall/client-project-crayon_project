9d86e0178c8774ee63f85b89f56a433939c38cec
MoonTea0416 <winter0416@hotmail.com>
2021-12-06 01:12:37 -0700

    congig

--- crayonApp/test.py
+++ crayonApp/test.py
@@ -6,10 +6,9 @@ def html_to_string(html, to_file):
     if platform.system() == 'Windows':
         pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
     else:
-       os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable) 
-       WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], 
-            stdout=subprocess.PIPE).communicate()[0].strip()
-       pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_PATH', '/app/bin/wkhtmltopdf')],
+        stdout=subprocess.PIPE).communicate()[0].strip()
+        pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
  
     pdfkit.from_string(html, to_file, configuration=pdfkit_config)
 
